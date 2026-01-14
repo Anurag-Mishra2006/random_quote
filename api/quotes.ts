cf891153efea5ed91e95d16bb5c7b7e28cc6976b
Anurag Mishra <eranurag2006@gmail.com>
2026-01-14 19:27:02 +0530

    FIX Vercel Api typescript build error

--- api/quotes.ts
+++ api/quotes.ts
@@ -1,10 +1,11 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
 export default async function handler(
-  req: VercelRequest,
-  res: VercelResponse
+  _req: Request
 ) {
   const response = await fetch("https://zenquotes.io/api/quotes");
   const data = await response.json();
-  res.status(200).json(data);
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 }
